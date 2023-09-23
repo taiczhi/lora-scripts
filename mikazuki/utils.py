4a82325823bc4e01e1ecb180fe2c69a88ed8083b
liefeng <1614656828@qq.com>
2023-09-23 19:15:19 +0800

    2023.8.08版本

---
+++ mikazuki/utils.py
@@ -0,0 +1,88 @@
+import os
+import sys
+import glob
+import subprocess
+import importlib.util
+from typing import Optional
+
+python_bin = sys.executable
+
+
+def check_training_params(data):
+    potential_path = [
+        "train_data_dir", "reg_data_dir", "output_dir"
+    ]
+    file_paths = [
+        "sample_prompts"
+    ]
+    for p in potential_path:
+        if p in data and not os.path.exists(data[p]):
+            return False
+
+    for f in file_paths:
+        if f in data and not os.path.exists(data[f]):
+            return False
+    return True
+
+
+def prepare_requirements():
+    requirements = [
+        "dadaptation", "prodigyopt"
+    ]
+    for r in requirements:
+        if not is_installed(r):
+            run_pip(f"install {r}", r)
+
+
+def get_total_images(path):
+    image_files = glob.glob(path + '/**/*.jpg', recursive=True)
+    image_files += glob.glob(path + '/**/*.jpeg', recursive=True)
+    image_files += glob.glob(path + '/**/*.png', recursive=True)
+    return len(image_files)
+
+
+def is_installed(package):
+    try:
+        spec = importlib.util.find_spec(package)
+    except ModuleNotFoundError:
+        return False
+
+    return spec is not None
+
+
+def run(command,
+        desc: Optional[str] = None,
+        errdesc: Optional[str] = None,
+        custom_env: Optional[list] = None,
+        live: Optional[bool] = True,
+        shell: Optional[bool] = False):
+    if desc is not None:
+        print(desc)
+
+    if live:
+        result = subprocess.run(command, shell=shell, env=os.environ if custom_env is None else custom_env)
+        if result.returncode != 0:
+            raise RuntimeError(f"""{errdesc or 'Error running command'}.
+Command: {command}
+Error code: {result.returncode}""")
+
+        return ""
+
+    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                            shell=shell, env=os.environ if custom_env is None else custom_env)
+
+    if result.returncode != 0:
+        message = f"""{errdesc or 'Error running command'}.
+Command: {command}
+Error code: {result.returncode}
+stdout: {result.stdout.decode(encoding="utf8", errors="ignore") if len(result.stdout) > 0 else '<empty>'}
+stderr: {result.stderr.decode(encoding="utf8", errors="ignore") if len(result.stderr) > 0 else '<empty>'}
+"""
+        raise RuntimeError(message)
+
+    return result.stdout.decode(encoding="utf8", errors="ignore")
+
+
+def run_pip(command, desc=None, live=False):
+    return run(f'"{python_bin}" -m pip {command}', desc=f"Installing {desc}", errdesc=f"Couldn't install {desc}", live=live)
+
